81034b86c26b4eb7b80cd14b0c3adc77ca85c86f
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2022-04-25 15:35:11 +0430

    visit tab added

--- js/customjs.js
+++ js/customjs.js
@@ -1,1 +1,8 @@
 // Tabs
+$('#btn-nav-previous').click(function() {
+    $(".menu-inner-box").animate({ scrollLeft: "+=100px" });
+});
+
+$('#btn-nav-next').click(function() {
+    $(".menu-inner-box").animate({ scrollLeft: "+=100px" });
+});
