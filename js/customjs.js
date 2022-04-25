0aa404283e1e13edd197541fc04ea8f1fbcc1853
lordvexer <h.nypd@yahoo.com>
2022-04-25 22:27:39 +0430

    oops

--- js/customjs.js
+++ js/customjs.js
@@ -1,6 +1,6 @@
 // Tabs
 $('#btn-nav-previous').click(function() {
-    $(".menu-inner-box").animate({ scrollLeft: "+=100px" });
+    $(".menu-inner-box").animate({ scrollLeft: "-=100px" });
 });
 
 $('#btn-nav-next').click(function() {
